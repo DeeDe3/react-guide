b333e7eca7b69bab8b574998603e761f091059cf
Andrico Sean Galeon <andricoseanwork@gmail.com>
2021-12-11 12:57:17 +0800

    adding button event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
+    const clickHandler = () =>{
+        console.log('clicked :', );
+    }
     
     /*
         Now props has the expense data, pass the props date data to ExpenseData
@@ -15,6 +18,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     )
 }
