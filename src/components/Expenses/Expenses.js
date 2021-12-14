734a2dafc5bb9cbb6d2dc522813d421c5b5cd242
Andrico Sean Galeon <andricoseanwork@gmail.com>
2021-12-14 10:38:25 +0800

    rendering list of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -38,31 +38,18 @@ const Expenses = () => {
         console.log('In Expense.js')
         console.log('year :', year);
         setYear(year);
-      }
+    }
 
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={year} onFilterYear={filterYear} />
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-            <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
+            {expenses.map((expense) => (
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
         </Card>
     )
 }
