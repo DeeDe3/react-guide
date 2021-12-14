223be92cad2edb905305d34af571319a8678ec00
Andrico Sean Galeon <andricoseanwork@gmail.com>
2021-12-14 13:04:22 +0800

    filter the year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,10 +12,14 @@ const Expenses = (props) => {
         setYear(year);
     }
 
+    const filterExpense = props.items.filter(expense =>{
+        return expense.date.getFullYear().toString() === year
+    })
+
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={year} onFilterYear={filterYear} />
-            {props.items.map((expense) => (
+            {filterExpense.map((expense) => (
                 <ExpenseItem
                     key={expense.id}
                     title={expense.title}
