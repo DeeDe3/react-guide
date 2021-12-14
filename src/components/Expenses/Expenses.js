c4246a26487b1f48d7246a1b750dec84aa0d525c
Andrico Sean Galeon <andricoseanwork@gmail.com>
2021-12-14 11:55:41 +0800

    working with keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,21 +4,6 @@ import ExpensesFilter from './ExpensesFilter'
 import { useState } from 'react'
 import Card from '../UI/Card'
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e3',
-        title: 'Health Insurance',
-        amount: 420.420,
-        date: new Date(2020, 7, 28)
-    },
-    {
-        id: 'e4',
-        title: 'KKK Membership',
-        amount: 420.69,
-        date: new Date(2021, 12, 28)
-    },
-]
-
 const Expenses = (props) => {
     const [year, setYear] = useState('2020')
 
@@ -32,6 +17,7 @@ const Expenses = (props) => {
             <ExpensesFilter selected={year} onFilterYear={filterYear} />
             {props.items.map((expense) => (
                 <ExpenseItem
+                    key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
