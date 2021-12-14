be9a4fcd5e377a7c19a13ded514021e0a0031a8e
Andrico Sean Galeon <andricoseanwork@gmail.com>
2021-12-14 13:34:09 +0800

    adding conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,19 @@
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem'
 import ExpensesFilter from './ExpensesFilter'
 import { useState } from 'react'
 import Card from '../UI/Card'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
     const [year, setYear] = useState('2020')
 
     const filterYear = year => {
-        console.log('year :', year);
         setYear(year);
     }
 
     const filterExpense = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === year
     })
-
-    let expensesContent = <p>No expenses found</p>
-    if (filterExpense.length > 0) {
-        expensesContent = filterExpense.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ))
-    }
-
     /* 
         If the conditoin is true, the next statement in the '&&' is outputted(Conditional Content). 
     */
@@ -37,18 +23,7 @@ const Expenses = (props) => {
                 selected={year}
                 onFilterYear={filterYear}
             />
-            {expensesContent}
-            {/* {filterExpense.length === 0 && <p>No expenses found</p>}
-            {filterExpense.length > 0 &&
-                filterExpense.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))
-            } */}
+            <ExpensesList items={filterExpense}/>
         </Card>
     )
 }
